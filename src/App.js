6902e133422e91fd9fa1b7708d9c93d983f11967
Matin Sarahi <matinsarahi@gmail.com>
2021-03-20 23:45:04 -0400

    hello

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hello hi hey
+          Learn React hello hi hey hey
         </a>
       </header>
     </div>
